502b3632364e1958db8138f3b62e46d9982b3869
Felix Vadcard <vadcard.felix@gmail.com>
2025-05-06 21:25:18 +0200

    first commit

---
+++ src/main/java/com/fvd/jooq/JobApplication.java
@@ -0,0 +1,63 @@
+package com.fvd.jooq;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
+import org.jooq.InsertValuesStepN;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@QuarkusMain
+public class JobApplication implements QuarkusApplication {
+
+  @Inject
+  @Named("dslAlt")
+  DSLContext dslAlt;
+  @Inject
+  DSLContext postgresDsl;
+
+  @Override
+  @SneakyThrows
+  public int run(String... args) throws Exception {
+    log.info("start");
+    //verify nothing is inside
+    dslAlt.meta().getSchemas("public")
+      .getFirst().getTables().forEach(table -> log.info("table in alt before : {}", table));
+    //migrate
+    postgresDsl.meta().getSchemas("public")
+      .getFirst().getTables().forEach(table -> {
+        log.info("table found : {}", table);
+        // Create table if not exist
+        dslAlt.createTableIfNotExists(table.getName())
+          .columns(table.fields())
+          .execute();
+        //Drop old data
+        dslAlt.dropTableIfExists(table)
+          .execute();
+        // Fetch records
+        List<Map<String, Object>> maps = postgresDsl.selectFrom(table).fetch().intoMaps();
+        //Insert records
+        InsertValuesStepN<?> insertStep = dslAlt.insertInto(table, table.fields());
+        maps.forEach(map ->
+          insertStep.values(map.values())
+        );
+        insertStep.execute();
+      });
+    //Verify
+    dslAlt.meta().getSchemas("public")
+      .getFirst().getTables().forEach(table -> {
+        log.info("table in alt after : {}", table);
+        dslAlt.selectFrom(table).fetch().forEach(record -> {
+          log.info("Record found ! {}", record);
+        });
+      });
+    log.info("end");
+    return 0;
+  }
+}
---
+++ src/test/java/com/fvd/jooq/JobApplicationTest.java
@@ -0,0 +1,15 @@
+package com.fvd.jooq;
+
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.QuarkusMainTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusMainTest
+class JobApplicationTest {
+
+  @Test
+  @Launch
+  void testApp() {
+  }
+
+}
